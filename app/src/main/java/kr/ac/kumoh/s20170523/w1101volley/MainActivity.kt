dfb99ee539e1f767b5c506e4caef642c48b6a269
W1002StringList <gmlghd98@gmail.com>
2022-11-14 16:12:18 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/s20170523/w1101volley/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20170523.w1101volley
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
